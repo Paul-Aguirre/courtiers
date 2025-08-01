828ed7959677fc870c646a05cf9b2b80824c32ea
Paul Aguirre <aguirre.paul6200@gmail.com>
2025-08-01 02:33:20 +0200

    Refactored the family status const into the FamilyStatus enum

--- engine/src/queens_table.rs
+++ engine/src/queens_table.rs
@@ -1,49 +1,74 @@
 use std::cmp::Ordering;
 use std::iter::zip;
 
-use crate::piles::{Piles, GetFamily};
 use crate::courtier_card::CourtierFamily;
 use crate::courtier_card::CourtierFamily::{Carp, Hare, Moth, Nightingale, Stag, Toad};
+use crate::piles::{GetFamily, Piles};
 
+pub type Result<T> = std::result::Result<T, Error>;
+#[derive(Debug)]
+enum Error {
+    FamilyStatusError { number: i8 },
+}
+pub enum FamilyStatus {
+    InTheLight,
+    Neutral,
+    Disgraced,
+}
 
-const IN_THE_LIGHT: i8 = 1;
-const NEUTRAL: i8 = 0;
-const DISGRACED: i8 = -1;
+impl FamilyStatus {
+    pub fn value(&self) -> i8 {
+        match self {
+            FamilyStatus::Disgraced => -1,
+            FamilyStatus::Neutral => 0,
+            FamilyStatus::InTheLight => 1,
+        }
+    }
+
+    fn from_number(n: i8) -> Result<FamilyStatus> {
+        match n {
+            -1 => Ok(FamilyStatus::Disgraced),
+            0 => Ok(FamilyStatus::Neutral),
+            1 => Ok(FamilyStatus::InTheLight),
+            _ => Err(Error::FamilyStatusError { number: n }),
+        }
+    }
+}
 
 pub struct FamiliesStatuses {
-    moths: i8,
-    toads: i8,
-    nightingales: i8,
-    hares: i8,
-    stags: i8,
-    carps: i8,
+    moths: FamilyStatus,
+    toads: FamilyStatus,
+    nightingales: FamilyStatus,
+    hares: FamilyStatus,
+    stags: FamilyStatus,
+    carps: FamilyStatus,
 }
 
 impl FamiliesStatuses {
-    fn from_array(a: [i8; 6]) -> Self {
-        FamiliesStatuses {
-            moths: a[0],
-            toads: a[1],
-            nightingales: a[2],
-            hares: a[3],
-            stags: a[4],
-            carps: a[5],
-        }
+    fn from_array(a: [i8; 6]) -> Result<Self> {
+        Ok(FamiliesStatuses {
+            moths: FamilyStatus::from_number(a[0])?,
+            toads: FamilyStatus::from_number(a[1])?,
+            nightingales: FamilyStatus::from_number(a[2])?,
+            hares: FamilyStatus::from_number(a[3])?,
+            stags: FamilyStatus::from_number(a[4])?,
+            carps: FamilyStatus::from_number(a[5])?,
+        })
     }
 
     pub fn as_array(&self) -> [i8; 6] {
         [
-            self.moths,
-            self.toads,
-            self.nightingales,
-            self.hares,
-            self.stags,
-            self.carps,
+            self.moths.value(),
+            self.toads.value(),
+            self.nightingales.value(),
+            self.hares.value(),
+            self.stags.value(),
+            self.carps.value(),
         ]
     }
 }
 
-impl_get_family!(FamiliesStatuses, i8);
+impl_get_family!(FamiliesStatuses, FamilyStatus);
 
 pub struct QueensTable {
     in_the_light: Piles,
@@ -60,11 +85,11 @@ impl QueensTable {
         .enumerate()
         {
             statuses_array[i] = match in_the_light_score.cmp(&disgraced_score) {
-                Ordering::Equal => NEUTRAL,
-                Ordering::Greater => IN_THE_LIGHT,
-                Ordering::Less => DISGRACED,
+                Ordering::Equal => FamilyStatus::Neutral.value(),
+                Ordering::Greater => FamilyStatus::InTheLight.value(),
+                Ordering::Less => FamilyStatus::Disgraced.value(),
             };
         }
-        FamiliesStatuses::from_array(statuses_array)
+        FamiliesStatuses::from_array(statuses_array).unwrap()
     }
 }
