69bda478665ff54817a0c49fba2d683185844e52
Paul Aguirre <aguirre.paul6200@gmail.com>
2025-08-05 03:17:37 +0200

    Added end_game(), refactored init_game() and next_player() adn added test

--- engine/src/game.rs
+++ engine/src/game.rs
@@ -1,7 +1,9 @@
+use std::{cell::RefCell, rc::Rc};
+
 use crate::{
-    courtier_card::{CourtierCard, build_deck},
-    player::Player,
-    queens_table::{self, QueensTable},
+    courtier_card::{self, CourtierCard},
+    player::{self, Player},
+    queens_table::QueensTable,
 };
 
 pub struct Game {
@@ -9,30 +11,61 @@ pub struct Game {
     players: Vec<Player>,
     queens_table: QueensTable,
     current_player_index: u8,
+    player_cycle_iter: Box<dyn Iterator<Item = usize>>,
 }
 
 impl Game {
-    fn init_game(player_names: Vec<String>) -> Result<Game, crate::courtier_card::Error> {
-        let deck = build_deck(player_names.len() as u8)?;
+    pub fn init_game(player_names: Vec<String>) -> Result<Game, crate::courtier_card::Error> {
+        // initialize deck of shuffle CourtierCard instances
+        let deck = courtier_card::build_deck(player_names.len() as u8)?;
+
+        // initialize Vec<Player> with empty hand, domain, mission_cards, and domain_scores
         let players = Player::init_players(player_names);
+
+        // distribute cards for a hand to each player
+        // ...
+        // distribute 2 MissionCard instances to each player
+        // ...
+
+        // initialize QueensTable instance with empty Piles instances and statuses
         let queens_table = QueensTable::new();
+
+        let mut player_cycle_iter = Box::new((0..players.len()).into_iter().cycle());
+
         Ok(Game {
             deck,
             players,
             queens_table,
-            current_player_index: 0,
+            current_player_index: player_cycle_iter.next().unwrap().try_into().unwrap(),
+            player_cycle_iter,
         })
     }
 
+    pub fn start_main_loop(&mut self) {
+        todo!()
+    }
+
+    pub fn end_game(&mut self) {
+        // determine family statuses
+        self.queens_table.determine_family_statuses();
+
+        // tally points from players domains
+        for player in &mut self.players {
+            player.compute_domain_score(self.queens_table.get_statuses().as_ref().unwrap());
+        }
+
+        // tally points from players mission cards
+        // TODO test this part, I'm unsure it'll work
+        let wrapped_game = Rc::new(RefCell::new(self));
+        for player in &mut wrapped_game.borrow_mut().players {
+            player.compute_missions_score(&wrapped_game.borrow_mut());
+        }
+
+        // determine and print the winner / total scores / score board
+    }
+
     fn next_player(&mut self) {
-        // TODO test this one !!!!!!!!!!!!
-        self.current_player_index = (0..self.players.len())
-            .into_iter()
-            .cycle()
-            .next()
-            .unwrap()
-            .try_into()
-            .unwrap();
+        self.current_player_index = self.player_cycle_iter.next().unwrap().try_into().unwrap();
     }
 
     pub fn get_current_player(&self) -> &Player {
@@ -70,7 +103,28 @@ mod tests {
     use super::*;
 
     #[test]
-    fn next_player_increments_current_player_index() {
+    fn next_player_call_increments_current_player_index() {
+        let player_names = vec![
+            String::from("Bob"),
+            String::from("Alice"),
+            String::from("Guido"),
+        ];
+        let mut game = Game::init_game(player_names).unwrap();
+        assert_eq!(game.current_player_index, 0);
+        game.next_player();
+        assert_eq!(game.current_player_index, 1);
+        game.next_player();
+        assert_eq!(game.current_player_index, 2);
+        game.next_player();
+        assert_eq!(game.current_player_index, 0);
+    }
+
+    #[test]
+    fn test_end_game() {
         todo!()
+        // determine family statuses
+        // tally points from players domains
+        // tally points from players mission cards
+        // determine and print the winner / total scores / score board
     }
 }
